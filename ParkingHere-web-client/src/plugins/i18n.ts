c8adb33e3ee07e08e3a85dceab8820ce17b8ee48
k3gr <k3grzesik@gmail.com>
2023-05-20 21:20:14 +0200

    Add tools and main view.

---
+++ ParkingHere-web-client/src/plugins/i18n.ts
@@ -0,0 +1,10 @@
+import { createI18n } from 'vue-i18n'
+import plPL from '@/locales/pl-PL.json'
+
+export default createI18n({
+  locale: 'pl-PL',
+  fallbackLocale: 'pl-PL',
+  messages: {
+    'pl-PL': plPL
+  }
+})
