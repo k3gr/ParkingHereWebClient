8e7ef5dab237bddaa28dbd286dcc4b788a36c89c
k3gr <k3grzesik@gmail.com>
2023-07-16 18:04:31 +0200

    Add registration Store

---
+++ ParkingHere-web-client/src/appModules/account/component/UserLoginFormComp.vue
@@ -0,0 +1,57 @@
+<template>
+  <div
+    class="login-form position-absolute top-50 start-50 translate-middle p-3 bg-dark col-12 col-sm-8 col-lg-6 col-xl-4"
+    data-bs-theme="dark"
+  >
+    <form>
+      <div class="d-flex align-items-center justify-content-center">
+        <div
+          class="person-icon position-relative rounded-circle border border-3 border-success m-3"
+        >
+          <font-awesome-icon
+            :icon="['fa', 'user']"
+            class="icon text-light position-absolute top-50 start-50 translate-middle"
+            size="2x"
+          />
+        </div>
+        <h2 class="h3 ps-3 border-start text-light">{{ $t('Login') }}</h2>
+      </div>
+      <div class="form-floating my-3">
+        <input type="email" class="form-control" id="email" :placeholder="$t('EmailAddress')" />
+        <label class="text-secondary-emphasis" for="email">{{ $t('EmailAddress') }}</label>
+      </div>
+      <div class="form-floating my-3">
+        <input type="password" class="form-control" id="password" :placeholder="$t('Password')" />
+        <label class="text-secondary-emphasis" for="password">{{ $t('Password') }}</label>
+      </div>
+      <div class="checkbox mb-3">
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input bg-success" id="submit" />
+          <label class="form-check-label text-light" for="submit">{{ $t('RememberMe') }}</label>
+        </div>
+      </div>
+      <div class="d-flex justify-content-center">
+        <button class="btn btn-lg btn-success col-12 col-sm-5 col-xxl-4" type="submit">
+          {{ $t('SignIn') }}
+        </button>
+      </div>
+      <div class="mt-3">
+        <span class="m-0 me-2 text-light">{{ $t('FirstTimeOnParkingHere') }}</span>
+        <router-link
+          class="text-decoration-none text-success d-block d-sm-inline"
+          :to="{ name: 'signup' }"
+          >{{ $t('SignUp') }}.</router-link
+        >
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.person-icon {
+  width: 80px;
+  height: 80px;
+}
+</style>
